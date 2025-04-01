d29c7914c4a9221067ee02a70fb2e63124ba3737
manu12perez <manu12perez@gmail.com>
2025-04-01 11:00:56 +0200

    Add project files.

---
+++ MvcCoreAzureStorage/Program.cs
@@ -0,0 +1,41 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using MvcCoreAzureStorage.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+/***********************************************************************************************************/
+string azureKeys = builder.Configuration.GetValue<string>("AzureKeys:StorageAccount");
+BlobServiceClient blobServiceClient = new BlobServiceClient(azureKeys);
+builder.Services.AddTransient<BlobServiceClient>(x => blobServiceClient);
+
+builder.Services.AddTransient<ServiceStorageFiles>();
+builder.Services.AddTransient<ServiceStorageBlobs>();
+/***********************************************************************************************************/
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
---
+++ MvcCoreAzureStorage/Services/ServiceStorageFiles.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+
+namespace MvcCoreAzureStorage.Services
+{
+    public class ServiceStorageFiles
+    {
+        private ShareDirectoryClient root;
+
+        public ServiceStorageFiles(IConfiguration configuration)
+        {
+            string keys = configuration.GetValue<string>("AzureKeys:StorageAccount");
+            ShareClient client = new ShareClient(keys, "ejemplofiles"); //Carpeta creada en Azure
+            this.root = client.GetRootDirectoryClient();
+        }
+
+        //METODO PARA RECUPERAR TODOS LOS FICHEROS DE LA RAIZ DE SHARED
+        public async Task<List<string>> GetFilesAsync()
+        {
+            List<string> files = new List<string>();
+            await foreach(ShareFileItem item in this.root.GetFilesAndDirectoriesAsync())
+            {
+                files.Add(item.Name);
+            }
+            return files;
+        }
+
+        //METODO PARA LEER EL CONTENIDO DE UN FILE
+        public async Task<string> ReadFileAsync(string fileName)
+        {
+            ShareFileClient fileClient = this.root.GetFileClient(fileName);
+            ShareFileDownloadInfo data = await fileClient.DownloadAsync();
+            Stream stream = data.Content;
+            string contenido = "";
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                contenido = await reader.ReadToEndAsync();
+            }
+            return contenido;
+        }
+
+        //METODO PARA SUBIR UN FILE
+        public async Task UploadFileAsync(string fileName, Stream stream)
+        {
+            ShareFileClient fileClient = this.root.GetFileClient(fileName);
+            await fileClient.CreateAsync(stream.Length);
+            await fileClient.UploadAsync(stream);
+        }
+
+        //METODO PARA ELIMINAR UN FILE
+        public async Task DeleteFileAsync(string fileName)
+        {
+            ShareFileClient fileClient = this.root.GetFileClient(fileName);
+            await fileClient.DeleteAsync();
+        }
+    }
+}
